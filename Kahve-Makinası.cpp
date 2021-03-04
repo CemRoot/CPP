3ac65ac91b6028bbbbe473c32d61dd3960c47214
Cem koyluoglu <61415601+CemRoot@users.noreply.github.com>
2021-03-04 19:02:13 +0300

    Coffe-Mach added

---
+++ Kahve-Makinası.cpp
@@ -0,0 +1,176 @@
+#include <iostream>
+#include <chrono>
+#include <ctime>
+#include <cmath>
+#include <thread>
+using namespace std;
+
+using namespace std::chrono_literals;
+
+int
+main ()
+{
+  
+int
+    sec;
+  
+cout << " [1]->SD1cak Kahve\n";
+  
+cout << " [2]->SD1cak C'ikulatalD1 Kahve\n";
+  
+cout << " [3]->Amerikano Kahve\n";
+  
+cout << " [4]->SC<tlC< Kahve\n";
+  
+ 
+cin >> sec;
+  
+ 
+switch (sec)
+    {
+    
+case 1:
+      
+cout << "SD1cak Kahve Kahve SeC'ilmiEtir.\n";
+      
+break;
+    
+case 2:
+      
+cout << "SD1cak C'ikulatalD1 Kahve SeC'ilmiEtir.\n";
+      
+break;
+    
+case 3:
+      
+cout << "Amerikano Kahve SeC'ilmiEtir.\n";
+      
+break;
+    
+case 4:
+      
+cout << "SC<tlC< Kahve SeC'ilmiEtir.\n";
+      
+break;
+    
+default:
+      
+cout << "YanlD1E bir seC'im yaptD1nD1z!\n";
+      
+return sec;
+    
+}
+  
+char
+    seker;
+  
+cout << "Seker istermisiniz ? E/H \n";
+  
+cin >> seker;
+  
+if (seker == 'E' || seker == 'e')
+    {
+      
+cout << "Eeker atD1lD1yor....\n";
+    
+}
+  
+  else
+    
+cout << "Seker istenmedi!\n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+ "     %20   \n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+ "     %30   \n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+    "     %40   \n";;
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+ "     %50   \n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+ "     %60   \n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+ "     %70   \n";
+  
+std::this_thread::sleep_for (1 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+ "     %80   \n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+ "     %90   \n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+ "     %95   \n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+ "     %98   \n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+ "     %99   \n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " Kahveniz hazD1rlanD1yor lC<tfen bekleyiniz!\n" 
+    "     %100   \n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout << " ========== AFD0YET OLSUN ==============\n" 
+    "         ===STARBUCKS TM===\n";
+  
+std::this_thread::sleep_for (2 s);
+  
+cout < "............End.........";
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
